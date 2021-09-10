e11cdcaff0b5070065e67cb0303203b2ee18a60e
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-10 11:42:10 -0700

    added working search bar

--- src/containers/NewsContainer.jsx
+++ src/containers/NewsContainer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Controls from '../components/app/NewsSearch/Controls';
 import LoadingComponent from '../components/app/NewsSearch/LoadingComponent';
 import NewsList from '../components/app/NewsSearch/NewsList';
 import { fetchArticles } from '../services/newsApi';
@@ -15,6 +16,18 @@ export default class NewsContainer extends Component {
       this.setState({ articles, loading: false });
     }
 
+    handleSearchChange = (event) => {
+      this.setState({ search: event.target.value });
+    }
+
+    handleSubmit = async (event) => {
+      event.preventDefault();
+
+      this.setState({ loading: true });
+      const articles = await fetchArticles(this.state.search);
+      this.setState({ articles, loading: false });
+    }
+
     render() {
       const { loading, articles, search } = this.state;
       if(loading) {
@@ -22,6 +35,9 @@ export default class NewsContainer extends Component {
       } else if(articles.length > 0) {
         return <>
           <h1>WELCOME TO {search.toUpperCase()} NEWS</h1>
+          <Controls search={search} 
+            onSearchChange={this.handleSearchChange} 
+            onSubmit={this.handleSubmit}/>
           <NewsList articles={articles} />
         </>;
         
